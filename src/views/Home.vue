cf06fb70621ecd4489da1d48534466102309bfc6
muzhangxion <254497987@qq.com>
2020-11-26 22:20:36 +0800

    图片颜色设置背景优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home" :style="{background: colors}">
-    <img id="tplWheelBg" alt="Vue logo" src="../assets/a.jpg">
+  <div class="home" >
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -16,56 +15,8 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 })
 
 export default class Home extends Vue {
-  colors = ''
-  mounted () {
-    this.aa()
-  }
 
-  aa () {
-    const canvas = document.createElement('canvas')
-    const imgs = document.getElementById('tplWheelBg')
-    console.log(canvas, imgs)
-    setTimeout(() => {
-      this.colors = this.setWheelBgtcMainTonal(canvas, imgs)
-    }, 200)
-    
-    
-  }
-
-  setWheelBgtcMainTonal(canvas: any, img: any) {
-        canvas.width = img.width
-        canvas.height = img.height
-        let context = canvas.getContext('2d')
-        context.drawImage(img, 0, 0)
-        // 获取像素数据
-        let data = context.getImageData(0, 0, img.width, img.height).data
-        console.log(data);
-        let r = 0,
-            g = 0,
-            b = 0
-        // 取所有像素的平均值
-        for (let row = 0; row < img.height; row++) {
-            for (let col = 0; col < img.width; col++) {
-                r += data[(img.width * row + col) * 4]
-                g += data[(img.width * row + col) * 4 + 1]
-                b += data[(img.width * row + col) * 4 + 2]
-            }
-        }
-        // 求取平均值
-        r /= img.width * img.height
-        g /= img.width * img.height
-        b /= img.width * img.height
-
-        // 将最终的值取整
-        r = Math.round(r)
-        g = Math.round(g)
-        b = Math.round(b)
-
-        //rgb转16进制 位运算
-        const color = `rgb(${r}, ${g}, ${b})`
-        // const color = ((r << 16) | (g << 8) | b).toString(16)
-        return color
-    }
+  mounted () {}
   
 }
 </script>
---
+++ src/views/ImageSetBackground.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="ImageSetBackground" :style="{background: colors_a}">
+    <img id="tplWheelBg_a" class="img_s" alt="Vue logo" src="../assets/a.jpg">
+  </div>
+  <div class="ImageSetBackground" :style="{background: colors_b}">
+    <img id="tplWheelBg_b" class="img_s" alt="Vue logo" src="../assets/b.jpg">
+  </div>
+  <div class="ImageSetBackground" :style="{background: colors_c}">
+    <img id="tplWheelBg_c" class="img_s" alt="Vue logo" src="../assets/c.jpg">
+  </div>
+  <div class="ImageSetBackground" :style="{background: colors_d}">
+    <img id="tplWheelBg_d" class="img_s" alt="Vue logo" src="../assets/d.jpg">
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+    
+  }
+})
+
+export default class ImageSetBackground extends Vue {
+  colors_a = ''
+  colors_b = ''
+  colors_c = ''
+  colors_d = ''
+  mounted () {
+    this.steBackcolor()
+  }
+
+  steBackcolor () {
+    const canvas = document.createElement('canvas')
+    const imgs_a = document.getElementById('tplWheelBg_a')
+    const imgs_b = document.getElementById('tplWheelBg_b')
+    const imgs_c = document.getElementById('tplWheelBg_c')
+    const imgs_d = document.getElementById('tplWheelBg_d')
+    setTimeout(() => {
+      this.colors_a = this.setWheelBgtcMainTonal(canvas, imgs_a)
+    }, 50)
+    setTimeout(() => {
+      this.colors_b = this.setWheelBgtcMainTonal(canvas, imgs_b)
+    }, 50)
+    setTimeout(() => {
+      this.colors_c = this.setWheelBgtcMainTonal(canvas, imgs_c)
+    }, 50)
+    setTimeout(() => {
+      this.colors_d = this.setWheelBgtcMainTonal(canvas, imgs_d)
+    }, 50)
+    
+    
+  }
+
+  setWheelBgtcMainTonal(canvas: any, img: any) {
+        canvas.width = img.width
+        canvas.height = img.height
+        let context = canvas.getContext('2d')
+        context.drawImage(img, 0, 0)
+        // 获取像素数据
+        let data = context.getImageData(0, 0, img.width, img.height).data
+        console.log(data);
+        let r = 0,
+            g = 0,
+            b = 0
+        // 取所有像素的平均值
+        for (let row = 0; row < img.height; row++) {
+            for (let col = 0; col < img.width; col++) {
+                r += data[(img.width * row + col) * 4]
+                g += data[(img.width * row + col) * 4 + 1]
+                b += data[(img.width * row + col) * 4 + 2]
+            }
+        }
+        // 求取平均值
+        r /= img.width * img.height
+        g /= img.width * img.height
+        b /= img.width * img.height
+
+        // 将最终的值取整
+        r = Math.round(r)
+        g = Math.round(g)
+        b = Math.round(b)
+
+        //rgb转16进制 位运算
+        const color = `rgb(${r}, ${g}, ${b})`
+        // const color = ((r << 16) | (g << 8) | b).toString(16)
+        return color
+    }
+  
+}
+</script>
+
+<style lang="less">
+ .img_s{
+    width: 500px;
+   height: auto;
+ }
+</style>
