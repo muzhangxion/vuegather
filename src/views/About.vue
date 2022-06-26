888d6f9de39f5b9426c6db9f10b2d3220d628918
muzhanggege <254497987@qq.com>
2022-06-26 22:57:49 +0800

    计算属性

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,11 @@
     <div>
       <h3>例子：</h3>
       <div>{{text.split(',').reverse().join(',')}}</div>
-      <div>{{text}}</div>
+      <div>{{textStr}}</div>
+      <div>{{name}}</div>
+       <div>{{firstName}} {{lastName}}</div>
     </div>
+
   </div>
 </template>
 
@@ -19,10 +22,30 @@ import { computed, reactive } from 'vue'
 
 export default class About extends Vue {
   text: any = '123,456';
+  // 原data
+  firstName = 'John'
+  lastName = 'Doe'
+
+  get textStr() {
+    return  this.text.split(',').reverse().join(',')
+  }
+
+  get name() {
+    return this.firstName + ' ' + this.lastName
+  }
+
+  set name(value) {
+    const splitted = value.split(' ')
+    this.firstName = splitted[0]
+    this.lastName = splitted[1] || ''
+    console.log(this.firstName,  this.lastName, 333)
+  }
 
   mounted () {
     console.log('About')
   }
+
+
 
 }
 </script>
